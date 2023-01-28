5a6bfdcec6a7404326e3815642c43a12fc25a716
emanuelprass <emanuelpras4@gmail.com>
2023-01-29 00:21:26 +0700

    index - naming convention and concetanation rules on php

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
-echo "Hello World";
+ //naming convention on php use snake case rule
+ $first_name = "Emanuel";
+ $last_name = "Prasetyawan";
+
+ //assignment operator
+ $full_name = "Emanuel";
+ $full_name .= " ";
+ $full_name .= "Prasetyawan";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <h1>Hi, Welcome 
+        <!-- concatenation on php use '.' -->
+        <?php echo  $first_name . " " . $last_name ?>
+    </h1>
+    <h2>Hi, Welcome
+        <?php echo  $full_name ?> 
+    </h2>
+</body>
+</html>
