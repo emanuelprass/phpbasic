f2b883eabadd48ddc55275c1f30b0ae8e3243ff0
emanuelprass <emanuelpras4@gmail.com>
2023-02-08 18:27:02 +0700

    constant - sample constant

---
+++ src/constant.php
@@ -0,0 +1,7 @@
+<?php
+    //create cons variable on php
+    //rules: first of character should be alphabeth and use UPPER_CASE
+    //define(constant_name, value, case_sensitive)
+    define("AUTHOR_NAME", "Emanuel Dina Prasetyawan", true);    
+    echo(AUTHOR_NAME);
+?>
